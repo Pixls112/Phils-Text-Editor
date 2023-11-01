9c0f80438c8088592fc8f5a2a34b2c069e39d806
Pixls112 <nguyen.phillip112@gmai.com>
2023-10-31 23:54:41 -0700

    first launch test:successful

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -26,7 +26,7 @@ export const putDb = async (content) => {
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET all from the jateDB');
-  const jateDb = await openDB('jateDB', 1);
+  const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
   const request = store.get(1);
